83971ded231a4cc71d6d8338340db0cac2ed88ce
Abhilash Nishane <abhilash.nishane6@gmail.com>
2025-01-01 00:43:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div className='heading-1'>Neev AI transforming unstructured data in doctor-patient conversation <br />into <br />insightful patient summary</div>
+      </div>
+      <div className='hero-container'>
+        <div className='img-container doc-img-con'>
+          <img src="https://static.vecteezy.com/system/resources/previews/002/805/084/large_2x/doctor-talking-to-patient-illustration-concept-free-vector.jpg" />
+          <div className='div1'>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className='neevai'>
+          <div className='neevai-text'>Neev AI</div>
+          <div className='report-img-con-2'>
+            <img src="https://www.neevhq.com/out.webp" />
+          </div>
+        </div>
+        <div className='img-container report-img-con'>
+          <img src="https://www.neevhq.com/out.webp" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
